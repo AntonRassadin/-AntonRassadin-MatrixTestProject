98c04c17e9e3b75e3a34bfb8dcf4f765d1301878
AntonRassadin <antonrsdn@gmail.com>
2020-02-19 14:38:45 +0300

    add snake list method

--- untitled7/src/Main.java
+++ untitled7/src/Main.java
@@ -3,25 +3,15 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-/*        TwoDemencialMatrix testTwoMatrix1 = new TwoDemencialMatrix();
+        TwoDemencialMatrix testTwoMatrix1 = new TwoDemencialMatrix();
         testTwoMatrix1.printMatrix();
         System.out.println("__________________");
-        TwoDemencialMatrix testTwoMatrix2 = new TwoDemencialMatrix(2,2);
-        testTwoMatrix2.printMatrix();
-        System.out.printf("Max: %d Min: %d \r\n", testTwoMatrix2.max(), testTwoMatrix2.min());
-        System.out.println("__________________");*/
-        //TwoDimMatrixWithZeroSum testZeroMatrix = new TwoDimMatrixWithZeroSum(4, 4);
-        TwoDimMatrixWithZeroSum testZeroMatrix = new TwoDimMatrixWithZeroSum();
-        testZeroMatrix.printMatrix();
-/*        try{
-            Field field=testZeroMatrix.getClass().getDeclaredField("nameMassive");
-            field.setAccessible(true);
-            System.out.println(field.get(testZeroMatrix));
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }*/
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        System.out.println("__________________");
+        TwoDemencialMatrix test2 = new TwoDemencialMatrix(5);
+        list.addAll(test2.toListSnake());
+        list.forEach(System.out::println);
+
 
 
         /*OneDemencialMatrix testMatrix1= new OneDemencialMatrix();
--- untitled7/src/TwoDemencialMatrix.java
+++ untitled7/src/TwoDemencialMatrix.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.lang.Math.random;
 
@@ -19,6 +20,14 @@ public class TwoDemencialMatrix implements MatrixInterface {
         createRandomArray();
     }
 
+/*
+    TwoDemencialMatrix(int tets){
+        array = new int[][]{{1,10,9},{2,11,8},{3,12,7},{4,5,6}};
+        this.amountFirstDim=4;
+        this.amountSecondDim=3;
+    }
+*/
+
     private void createRandomArray(){
         array = new int[amountFirstDim][amountSecondDim];
         for(int i=0; i<array.length; i++) {
@@ -36,6 +45,39 @@ public class TwoDemencialMatrix implements MatrixInterface {
         }
     }
 
+    public List<Integer> toListSnake(){
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        int count = amountFirstDim * amountSecondDim;
+        int x = 0;
+        int y = amountFirstDim - 1;
+
+        while (count > 0){
+            //right
+            for (int i = x; i < amountFirstDim-x; i++){
+                intList.add(array[i][x]);
+                count--;
+            }
+            x++;
+            //down
+            for (int i = x; i <= amountSecondDim - x; i++){
+                intList.add(array[y][i]);
+                count--;
+            }
+            //left
+            for (int i = y-x; i >= x; i--){
+                intList.add(array[i][amountSecondDim-x]);
+                count--;
+            }
+
+            //up
+            for (int i = amountSecondDim-x; i >= x; i--){
+                intList.add(array[x-1][i]);
+                count--;
+            }
+        }
+
+        return intList;
+    }
 
     protected void printName(){
         System.out.println(nameMassive);
@@ -50,4 +92,6 @@ public class TwoDemencialMatrix implements MatrixInterface {
     public int max() {
         return Arrays.stream(array).flatMapToInt(x -> Arrays.stream(x)).max().getAsInt();
     }
+
+
 }
